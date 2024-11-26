865664406dcc1f0191b82ee1a50612bed5ab133f
sarvajeet-hue <sarvajeet.1923en1095@kiet.edu>
2024-11-26 18:40:35 +0530

    updated code

--- src/components/Tictactoe.jsx
+++ src/components/Tictactoe.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 
 export const Tictactoe = () => {
-  
-  
-  const [blocks , setBlocks] = useState(new Array(9).fill(null))
-  const [currentPlayer , setCurrentPlayer] = useState("X")
+  const [blocks, setBlocks] = useState(new Array(9).fill(null));
+  const [currentPlayer, setCurrentPlayer] = useState("X");
 
   const board_Design = [
     [0, 1, 2],
@@ -17,34 +15,36 @@ export const Tictactoe = () => {
     [2, 4, 6],
   ];
   function handleClick(index) {
+    currentPlayer === "X" ? setCurrentPlayer("O") : setCurrentPlayer("X");
 
-      currentPlayer === "X" ? setCurrentPlayer("O") : setCurrentPlayer("X")
-    
     setBlocks((prevBlock) => {
-         
-        const newBlock = [...prevBlock]
-        
-        newBlock[index] = currentPlayer
+      const newBlock = [...prevBlock];
 
-        return newBlock
-    })
-
-    
+      newBlock[index] = currentPlayer;
+      
+      return newBlock;
+    });
   }
 
   return (
-    <div className="grid grid-cols-3 grid-rows-3  ">
-      {blocks.map((item , index) => {
-        return (
-          <div
-            onClick={() => handleClick(index)}
-            key={index}
-            className="border-black border h-[80px] w-[80px] flex items-center justify-center text-4xl "
-          >
-            {item}
-          </div>
-        );
-      })}
+    <div className="flex items-center justify-center flex-col gap-4">
+      <div className="grid grid-cols-3 grid-rows-3  ">
+        {blocks.map((item, index) => {
+          return (
+            <div
+              onClick={() => handleClick(index)}
+              key={index}
+              className={`border-black border h-[80px] w-[80px] flex items-center justify-center text-4xl ` }
+            >
+              {item}
+            </div>
+          );
+        })}
+      </div>
+
+      <div>
+        CurrentPlayer - {currentPlayer}
+      </div>
     </div>
   );
 };
