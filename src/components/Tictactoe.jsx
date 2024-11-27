5eb8da7ca0e4b07ae8c656f80cc8ae81adcc9e34
sarvajeet-hue <sarvajeet.1923en1095@kiet.edu>
2024-11-27 18:56:31 +0530

    updated code

--- src/components/Tictactoe.jsx
+++ src/components/Tictactoe.jsx
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 export const Tictactoe = () => {
   const [blocks, setBlocks] = useState(new Array(9).fill(null));
   const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [winner , setWinner] = useState('')
+  const [count  , setCount] = useState(0)
 
   const board_Design = [
     [0, 1, 2],
@@ -13,22 +15,45 @@ export const Tictactoe = () => {
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6],
-  ];
-  function handleClick(index) {
-    currentPlayer === "X" ? setCurrentPlayer("O") : setCurrentPlayer("X");
 
+
+  ];
+
+  function check_Game(index){
+    console.log("this function runs")
+      if(blocks[index] != null){
+        console.log("count")
+        setCount(count+1)
+      }
+
+      else if(count === 9){
+        setWinner("Draw")
+      }
+      
+    }
+  
+  
+  function handleClick(index) {
+    check_Game(index)
+      
+    if(blocks[index] != null){
+      return
+    }
     setBlocks((prevBlock) => {
+     
       const newBlock = [...prevBlock];
 
       newBlock[index] = currentPlayer;
       
       return newBlock;
     });
+    currentPlayer === "X" ? setCurrentPlayer("O") : setCurrentPlayer("X");
+    
   }
 
   return (
     <div className="flex items-center justify-center flex-col gap-4">
-      <div className="grid grid-cols-3 grid-rows-3  ">
+      <div className="grid grid-cols-3 grid-rows-3  ">  
         {blocks.map((item, index) => {
           return (
             <div
@@ -37,6 +62,7 @@ export const Tictactoe = () => {
               className={`border-black border h-[80px] w-[80px] flex items-center justify-center text-4xl ` }
             >
               {item}
+              
             </div>
           );
         })}
@@ -44,6 +70,9 @@ export const Tictactoe = () => {
 
       <div>
         CurrentPlayer - {currentPlayer}
+      </div>
+      <div>
+        Winner - {winner}
       </div>
     </div>
   );
