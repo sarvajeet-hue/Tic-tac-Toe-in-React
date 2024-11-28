d929405d918d20c63a2ddb234610530ac078de0c
sarvajeet-hue <sarvajeet.1923en1095@kiet.edu>
2024-11-28 18:30:09 +0530

    Final code

--- src/components/Tictactoe.jsx
+++ src/components/Tictactoe.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const Tictactoe = () => {
   const [blocks, setBlocks] = useState(new Array(9).fill(null));
   const [currentPlayer, setCurrentPlayer] = useState("X");
-  const [winner , setWinner] = useState('')
-  const [count  , setCount] = useState(0)
+  const [winner, setWinner] = useState("");
+  const [count, setCount] = useState(0);
 
   const board_Design = [
     [0, 1, 2],
@@ -15,65 +15,58 @@ export const Tictactoe = () => {
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6],
-
-
   ];
 
-  function check_Game(index){
-    console.log("this function runs")
-      if(blocks[index] != null){
-        console.log("count")
-        setCount(count+1)
-      }
+  useEffect(() => {
+    check_winner()
+  }, [blocks]);
 
-      else if(count === 9){
-        setWinner("Draw")
+  function check_winner() {
+    for(const combination of board_Design){
+      const [a , b, c] = combination // [0 , 1 , 2]
+      if(blocks[a] && blocks[a] === blocks[b] && blocks[a] === blocks[c] ){
+         setWinner(blocks[a])
       }
-      
     }
-  
-  
+ 
+    if(blocks.every((block) => block != null )){
+      setWinner("Draw")
+    }
+  }
+
   function handleClick(index) {
-    check_Game(index)
-      
-    if(blocks[index] != null){
-      return
+    if (blocks[index] != null || winner) {
+      return;
     }
-    setBlocks((prevBlock) => {
-     
-      const newBlock = [...prevBlock];
 
+    setBlocks((prevBlock) => {
+      const newBlock = [...prevBlock];
       newBlock[index] = currentPlayer;
-      
       return newBlock;
     });
+
     currentPlayer === "X" ? setCurrentPlayer("O") : setCurrentPlayer("X");
-    
+    check_winner();
   }
 
   return (
     <div className="flex items-center justify-center flex-col gap-4">
-      <div className="grid grid-cols-3 grid-rows-3  ">  
+      <div className="grid grid-cols-3 grid-rows-3  ">
         {blocks.map((item, index) => {
           return (
             <div
               onClick={() => handleClick(index)}
               key={index}
-              className={`border-black border h-[80px] w-[80px] flex items-center justify-center text-4xl ` }
+              className={`border-black border h-[80px] w-[80px] flex items-center justify-center text-4xl `}
             >
               {item}
-              
             </div>
           );
         })}
       </div>
 
-      <div>
-        CurrentPlayer - {currentPlayer}
-      </div>
-      <div>
-        Winner - {winner}
-      </div>
+      <div>CurrentPlayer - {currentPlayer}</div>
+      <div>Winner - {winner}</div>
     </div>
   );
 };
